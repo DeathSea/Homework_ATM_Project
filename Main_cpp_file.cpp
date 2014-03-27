ebab251fc2b3d661399b77b7460bb0ab5234a389
DeathSea <szmsdavid_wang@163.com>
2014-03-27 12:44:47 +0800

    Main cpp File

--- Main_cpp_file.cpp
+++ Main_cpp_file.cpp
@@ -5,9 +5,15 @@
 //using namespace std;
 int main(void)
 {
+	OutputText OutputText;
+	InputText  InputText;
 	enum Menu {MainMenu,ATMMenu,AdminMenu};
-	OutputText OutPutText;
-	OutPutText.Menu(MainMenu);
-    OutPutText.Menu(ATMMenu);
+	unsigned int Menu_length = 0;
+	unsigned int Choice      = 0;
+	Menu_length = OutputText.Menu(MainMenu);
+	Choice      = InputText.Choice(Menu_length);
+	//cout << Choice ;
+    //OutPutText.Menu(ATMMenu);
+	//OutPutText.Menu(AdminMenu);
     return 0;
 }
