fd7b9cabb66e86b1bf72f4b794207d23b01a562e
DeathSea <szmsdavid_wang@163.com>
2014-03-31 23:08:34 +0800

    Control layer Head

---
+++ Control.h
@@ -0,0 +1,36 @@
+/*
+¿ØÖÆ²ã£¨Control Layer£©
+Designed by DeathSea 2014 Jnu
+³ÌÐòÖ÷ÒªÂß¼­
+*/
+#include <stdexcept>
+class ATM
+{     
+public:
+	bool Login();
+//	void UserBalance();
+//	void Withdrawal();//#È¡¿î
+//	void Deposit();//#´æ¿î
+//	void Transfer();//#×ªÕË
+//	void ChangeCode(); 
+};
+class Admin
+{        
+public:
+	bool Login();
+//	void AddUser();
+//	void EXportUser();
+//	void ImportUser();
+//	void InquiryAllUserInfo();
+//	void InquiryNominatedUserInfo();
+//	void UserWithdrawal();
+//	void UserDeposit();
+//	void UserTransfer();
+//	void UserChangCode();
+};
+class Start
+{
+public:
+	void start();
+//	void show_menu();
+};
